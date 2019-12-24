0b1b8cc25d6b2162e14618fcdea505bef5835ce2
Gian Hiltbrunner <gianhiltbrunner@Gians-MacBook-Pro.local>
2019-12-24 21:42:07 +0100

    first cmmit

---
+++ README.md
@@ -0,0 +1,8 @@
+## sftp_dl
+
+This python script allows for interactive data downloads over SFTP. 
+
+### Usage
+```
+scpdl.py [-h] --host HOST --user USER [--pw PW]
+```
---
+++ scpdl.py
@@ -0,0 +1,70 @@
+import argparse
+import paramiko
+import os, sys
+from stat import *
+
+parser = argparse.ArgumentParser(description="Interactive SCP download")
+parser.add_argument("--host", required=True,
+                    help="HOSTNAME")
+parser.add_argument("--user", required=True,
+                    help="USERNAME")
+parser.add_argument("--pw", required=False,
+                    help="PASSWORD")
+args = parser.parse_args()
+
+if not args.pw:
+    password = input('Password:')
+else:
+    password = args.pw
+
+try:
+    t = paramiko.Transport(( args.host ))
+    t.connect(
+        username = args.user,
+        password = password,
+    )
+    sftp = paramiko.SFTPClient.from_transport(t)
+except:
+    print('Could not connect!')
+
+select = True
+while select:
+    path = '.'
+    dirlist = sftp.listdir(path)
+    attrlist = sftp.listdir_attr(path)
+
+    types = dict()
+    for i, file in enumerate(attrlist):
+        if S_ISDIR(file.st_mode): types[i] = 'DIR'
+        if S_ISLNK(file.st_mode): types[i] = 'LNK'
+        if S_ISREG(file.st_mode): types[i] = 'REG'
+
+    items = dict()
+    types_item_id = dict()
+    item_id = 0
+    for i, el in enumerate(dirlist):
+        if not el[0] == '.':
+            item_id += 1
+            
+            items[item_id] = el
+            types_item_id[item_id] = types[i]
+
+            print(item_id,':' ,types[i], ':', el)
+
+    select = input('Enter the file/folder, ".." to go one level up or 0 to quit:')
+
+    def progress(transferred, total):
+        if transferred == total:
+            print('100 % ...done!')
+        sys.stdout.write("%d%%   \r" % (transferred / total * 100) )
+
+    if select == '..':
+        sftp.chdir(path='..')
+    elif select.isdigit():
+        if int(select):
+            select = int(select)
+            if types_item_id[select] == 'DIR' or types_item_id[select] == 'LNK':
+                sftp.chdir(path='./' + items[int(select)])
+            elif types_item_id[select] == 'REG':
+                print('Downloading ' + items[int(select)])
+                sftp.get(remotepath='./' + items[int(select)], localpath = './' + items[int(select)], callback = progress)
